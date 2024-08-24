b9bd0736874d9ce279559bc8e88a5ce9d78550e1
Sandip Maity <sandipmaity21052003@gmail.com>
2024-08-24 11:56:42 +0530

    "updt"

--- app/components/WalletConnector.js
+++ app/components/WalletConnector.js
@@ -22,24 +22,29 @@ const WalletConnector = ({ onConnect }) => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-500 to-blue-500">
-      <div className="p-8 bg-white rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105">
-        <h1 className="text-2xl font-bold text-center mb-6">
-          Connect Your Wallet
-        </h1>
-        <button
-          onClick={connectWallet}
-          className={`w-full py-2 px-4 text-white font-semibold rounded-lg transition-colors duration-300 ${
-            loading
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-600 hover:bg-blue-700"
-          }`}
-          disabled={loading}
-        >
-          {loading ? "Connecting..." : "Connect Wallet"}
-        </button>
+    <>
+      <div className="text-center bg-gradient-to-r from-purple-500 to-blue-500 py-8">
+        <h1 className="text-5xl font-bold">Welcome to CryptoFlip!</h1>
       </div>
-    </div>
+      <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-500 to-blue-500">
+        <div className="p-8 bg-white rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105">
+          <h1 className="text-2xl font-bold text-center text-fuchsia-800 mb-6">
+            Connect Your Wallet
+          </h1>
+          <button
+            onClick={connectWallet}
+            className={`w-full py-2 px-4 text-white font-semibold rounded-lg transition-colors duration-300 ${
+              loading
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-blue-600 hover:bg-blue-700"
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Connecting..." : "Connect Wallet"}
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
